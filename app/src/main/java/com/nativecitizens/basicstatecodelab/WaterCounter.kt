c5a771e9ac66f5297e449748df3ce316abed8073
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2022-11-29 00:25:41 +0100

    feat(): Make count a state for compose to track and recompose the UI dependent on count

--- app/src/main/java/com/nativecitizens/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/nativecitizens/basicstatecodelab/WaterCounter.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,13 +13,19 @@ import com.nativecitizens.basicstatecodelab.ui.theme.BasicStateCodeLabTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
+    var count by remember {
+        mutableStateOf(0)
+    }
+
     Column(modifier = modifier.padding(16.dp)) {
-        Text(
-            text = "You've had $count glasses."
-        )
+        if (count > 0){
+            Text(
+                text = "You've had ${count} glasses."
+            )
+        }
         ElevatedButton(
-            onClick = {},
+            onClick = {count++},
+            enabled = count < 10,
             modifier = Modifier.padding(vertical = 8.dp)
         ) {
             Text(text = "Add one")
