e79596957092b3231822103b99ed6e5d9fe20ace
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2022-11-28 22:28:02 +0100

    feat: WaterCounter implemented

--- app/src/main/java/com/nativecitizens/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/nativecitizens/basicstatecodelab/WaterCounter.kt
@@ -1,2 +1,28 @@
 package com.nativecitizens.basicstatecodelab
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nativecitizens.basicstatecodelab.ui.theme.BasicStateCodeLabTheme
+
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewWaterCounter(){
+    BasicStateCodeLabTheme {
+        WaterCounter(Modifier.padding(16.dp))
+    }
+}
